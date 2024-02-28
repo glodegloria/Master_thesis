9df184172ee15fd5635e6943708276608c02cc8a
glodegloria <gloriuchi22@gmail.com>
2024-02-28 10:03:30 +0100

    He cambiado el archivo illogical changes

--- illogical_changes.py
+++ illogical_changes.py
@@ -5,6 +5,11 @@ def main(modis_stack, change_map, years, etiqueta2):
     cambio=[[0 for k in range(5)] for o in range(5)]
     come=[[0 for k in range(5)] for o in range(5)]
 
+    
+    #create a list that compares each year with the year before
+    #in cambio it saves the pixels that in the beginning they were this landcover and it stop being this landcover
+    #in come it saves the pixels that in the beginning they weren't this landcover and it start being this landcover
+    
     for a in range(len(years)-1):
         for i in range(483):
             for j in range(689):
@@ -12,6 +17,7 @@ def main(modis_stack, change_map, years, etiqueta2):
                     cambio[modis_stack[a][0,i,j]-1][modis_stack[a+1][0,i,j]-1]+=1
                     come[modis_stack[a+1][0,i,j]-1][modis_stack[a][0,i,j]-1]+=1
 
+    #a time series with the number of pixels they have
     list_time_series=[[0 for k in range(len(years))] for i in range(len(etiqueta2))]
     for a in range(len(years)):
         for i in range(483):
@@ -22,6 +28,7 @@ def main(modis_stack, change_map, years, etiqueta2):
     landcover=list(range(5))
     X_axis = np.arange(len(landcover)) 
 
+    #plots with the bar plots and the graphics of the time series
     for i in range(5):
         plt.subplot(121)
         plt.plot(years,np.array(list_time_series[i], dtype=int))
@@ -40,21 +47,26 @@ def main(modis_stack, change_map, years, etiqueta2):
         plt.suptitle("Landcover change of "+etiqueta2[i])
         plt.show()
 
+    #a list that saves the positions of the illogical transformations
     illogical_positions=[]
+    illogical_changes=[]
     for a in range(len(years)-1):
         for i in range(483):
             for j in range(689):
                 if modis_stack[a][0,i,j]==1 and modis_stack[a+1][0,i,j]==4:
-                    print("En la posicion ("+str(i)+","+str(j)+") ha cambiado de forest a waterbody")
+                    print("In the position ("+str(i)+","+str(j)+"), it has changed from forest to waterbody")
                     illogical_positions.append([years[a],i,j])
                 elif modis_stack[a][0,i,j]==2 and modis_stack[a+1][0,i,j]==4:
-                    print("En la posicion ("+str(i)+","+str(j)+") ha cambiado de grassland a waterbody")
+                    print("In the position ("+str(i)+","+str(j)+"), it has changed from grassland to waterbody")
                     illogical_positions.append([years[a],i,j])
                 elif modis_stack[a][0,i,j]==3 and modis_stack[a+1][0,i,j]==4:
-                    print("En la posicion ("+str(i)+","+str(j)+") ha cambiado de cropland a waterbody")
+                    print("In the position ("+str(i)+","+str(j)+"), it has changed from cropland to waterbody")
                     illogical_positions.append([years[a],i,j])
                 elif modis_stack[a][0,i,j]==4 and modis_stack[a+1][0,i,j]==1:
-                    print("En la posicion ("+str(i)+","+str(j)+") ha cambiado de waterbody a forest")
+                    print("In the position ("+str(i)+","+str(j)+"), it has changed from waterbody to forest")
+                    illogical_positions.append([years[a],i,j])
+                elif modis_stack[a][0,i,j]==5 and modis_stack[a+1][0,i,j]==1:
+                    print("In the position ("+str(i)+","+str(j)+"), it has changed from built-up to forest")
                     illogical_positions.append([years[a],i,j])
     try:
         return(illogical_positions)
