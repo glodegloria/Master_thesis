2524bffd73f2c4d60ac6bf1ea00056bd20958199
glodegloria <gloriuchi22@gmail.com>
2024-02-28 10:25:05 +0100

    Changes in cosa, adding the new inputs

--- cosa.py
+++ cosa.py
@@ -16,24 +16,44 @@ def illogical_positions(modis_stack, years):
                     ilpo.append([years[a],i,j])
     return ilpo
 
-def table_X(rf, ilpo, modis_stack_NDVI, years, modis_stack_drought, modis_stack_elevation, modis_stack_DSMP):
-    X_illo=[["NDVI","Drought","Elevation","DSMP"]]
+def table_X(rf, ilpo, modis_stack_NDVI, years, modis_stack_population, modis_stack_radiation, modis_stack_DSMP, modis_stack_precipitation, modis_stack_temperature, modis_stack_production, modis_stack_fragmentation):
+    from cosa import main as ReductionIllogical
+
+    X_illo=[["NDVI","DSMP","Population","Radiation","Precipitation","Temperature","Production","Fragmentation"]]
     for i in range(len(ilpo)):
         r=[]
         if math.isnan(float(modis_stack_NDVI[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
             r.append(modis_stack_NDVI[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
         if math.isnan(float(modis_stack_NDVI[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
             r.append(0)
-        r.append(modis_stack_drought[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
-        r.append(modis_stack_elevation[0,ilpo[i][1],ilpo[i][2]])
         r.append(modis_stack_DSMP[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        r.append(modis_stack_population[0][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_radiation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
+            r.append(modis_stack_radiation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_radiation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
+            r.append(0)
+        if math.isnan(float(modis_stack_precipitation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
+            r.append(modis_stack_precipitation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_precipitation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
+            r.append(0)
+        if math.isnan(float(modis_stack_temperature[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
+            r.append(modis_stack_temperature[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_temperature[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
+            r.append(0)
+        if math.isnan(float(modis_stack_production[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
+            r.append(modis_stack_production[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_production[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
+            r.append(0)
+        if math.isnan(float(modis_stack_fragmentation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==False:
+            r.append(modis_stack_fragmentation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]])
+        if math.isnan(float(modis_stack_fragmentation[years.index(ilpo[i][0])][0,ilpo[i][1],ilpo[i][2]]))==True:
+            r.append(0)
         X_illo.append(r)
+        nombres_columnas = X_illo[0]
 
-    nombres_columnas = X_illo[0]
+        X_rata = pd.DataFrame(X_illo[1:], columns=nombres_columnas)
 
-    X_illo = pd.DataFrame(X_illo[1:], columns=nombres_columnas)
-
-    Y=rf.predict(X_illo)
+    Y=rf.predict(X_rata)
     
     return Y
 
@@ -44,29 +64,37 @@ def NumberIllogical(ilpo, modis_stack, years, Y):
             num_iltra+=1
     return num_iltra
 
-def main(rf, years, modis_stack, modis_stack_NDVI, modis_stack_drought, modis_stack_elevation, modis_stack_DSMP):
+def main(rf, years, modis_stack, modis_stack_NDVI, modis_stack_population, modis_stack_radiation, modis_stack_DSMP, modis_stack_precipitation, modis_stack_temperature, modis_stack_production, modis_stack_fragmentation):
     
+    print ("aaaaaaaaa")
+    ilpo=[]
+    illo=illogical_positions(modis_stack, years)
+    ilpo.append(illogical_positions(modis_stack, years))
 
-    ilpo=illogical_positions(modis_stack, years)
+    Y=table_X(rf, illo, modis_stack_NDVI, years, modis_stack_population, modis_stack_radiation, modis_stack_DSMP, modis_stack_precipitation, modis_stack_temperature, modis_stack_production, modis_stack_fragmentation)
 
-    Y=table_X(rf, ilpo, modis_stack_NDVI, years, modis_stack_drought, modis_stack_elevation, modis_stack_DSMP)
-
-    num_iltra=NumberIllogical(ilpo, modis_stack, years, Y)   
 
     modis_stack_final=modis_stack
-    for i in ilpo:
-        modis_stack_final[years.index(i[0])][0,i[1],i[2]]=Y[ilpo.index(i)] 
+    #print(modis_stack_final[years.index(illo[0][0])][0,illo[0][1],illo[0][2]])
+    for i in illo:
+        modis_stack_final[years.index(i[0])][0,i[1],i[2]]=Y[illo.index(i)]
 
-    while len(ilpo) > 40:
-        ilpo=illogical_positions(modis_stack_final, years)
-        print(ilpo[1])
-        Y=table_X(rf, ilpo, modis_stack_NDVI, years, modis_stack_drought, modis_stack_elevation, modis_stack_DSMP)
-        num_iltra=NumberIllogical(ilpo, modis_stack_final, years, Y)
-        print("There are"+str(len(ilpo))+" illogical positions")
-        for i in ilpo:
-            modis_stack_final[years.index(i[0])][0,i[1],i[2]]=Y[ilpo.index(i)]
     
-    return modis_stack_final
+    j=0
+
+    while len(ilpo[j]) > 40 and j<60:
+        illo=illogical_positions(modis_stack_final, years)
+        ilpo.append(illo)
+        print(str(ilpo[j][1])+"aaa")
+        Y=table_X(rf, illo, modis_stack_NDVI, years, modis_stack_population, modis_stack_radiation, modis_stack_DSMP, modis_stack_precipitation, modis_stack_temperature, modis_stack_production, modis_stack_fragmentation)
+        #num_iltra=NumberIllogical(ilpo[j], modis_stack_final, years, Y)
+        print("There are"+str(len(ilpo[j]))+" illogical positions")
+        for i in illo:
+            
+            modis_stack_final[years.index(i[0])][0,i[1],i[2]]=Y[illo.index(i)]
+        j+=1
+    
+    return modis_stack_final, ilpo
 
     
         
